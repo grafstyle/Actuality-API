7feee45d57294c2276934d3f7bbaff3f110d69aa
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-01 01:53:04 -0400

    Creating DB module with simple methods

---
+++ src/DB.js
@@ -0,0 +1,12 @@
+import { MongoClient } from "mongodb";
+
+let conn;
+
+export function connect() {
+  conn = new MongoClient("mongodb://localhost:27017");
+  return conn.db("Actuality");
+}
+
+export function close() {
+  conn.close();
+}
