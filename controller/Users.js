58f31c5d0599e2a6022d4acb0aa6a11d882932dd
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-04 01:23:14 -0400

    Updating methods and adding codification

--- controller/Users.js
+++ controller/Users.js
@@ -1,13 +1,18 @@
 "use strict";
+import { encode } from "../middleware/auth.js";
 import { connect } from "../src/DB.js";
 export const table = connect().collection("Users");
 
-export async function getUser(data) {
-  if (data == undefined) return table.find().toArray();
-  return table.find(data).toArray();
+let dataRet;
+
+export async function getUsers(data) {
+  if (data == undefined) data = {};
+  dataRet = await table.find(data).toArray();
+  return encode(dataRet);
 }
 
 export async function getOneUser(data) {
-  if (data == undefined) return table.findOne();
-  return table.findOne(data);
+  if (data == undefined) data = {};
+  dataRet = await table.findOne(data);
+  return encode(dataRet);
 }
