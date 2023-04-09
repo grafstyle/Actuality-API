036f5e89ed10ebea7dacf4a3acd63f468dd8f14a
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-08 22:13:09 -0400

    Creating the comment schema

---
+++ models/Comments.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const CommentSchema = {
+  id_user: Number,
+  id_post: Number,
+  comment: String,
+  media: String,
+  date_adding: String,
+  date_modified: String,
+};
+
+export default CommentSchema;
