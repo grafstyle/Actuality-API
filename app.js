1c07efd8f0a6149a5f0843a0eece583303998f72
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-09 02:06:16 -0400

    Adding GET, POST, UPDATE, DELETE methods to comments

--- app.js
+++ app.js
@@ -12,12 +12,19 @@ import {
   getPosts,
   updatePost,
 } from "./controller/Posts.js";
+import {
+  addComment,
+  deleteComment,
+  getComments,
+  updateComment,
+} from "./controller/Comments.js";
 
 const app = new App();
 const router = new Router();
 
 let allDataUsers = decode(await getUsers());
 let allDataPosts = decode(await getPosts());
+let allDataComments = decode(await getComments());
 
 allDataUsers.forEach((elem) => {
   Object.keys(elem).forEach((key) => {
@@ -41,8 +48,22 @@ allDataPosts.forEach((elem) => {
   });
 });
 
+allDataComments.forEach((elem) => {
+  Object.keys(elem).forEach((key) => {
+    if (key == "id") {
+      let withIdUrl = "/Comments/" + elem["id"];
+      router.get(withIdUrl, elem);
+      router.update(withIdUrl, elem["id"], updateComment);
+      router.delete(withIdUrl, elem["id"], deleteComment);
+    }
+  });
+});
+
 router.get("/Users", allDataUsers);
 router.post("/Users", addUser);
 
 router.get("/Posts", allDataPosts);
 router.post("/Posts", addPost);
+
+router.get("/Comments", allDataComments);
+router.post("/Comments", addComment);
