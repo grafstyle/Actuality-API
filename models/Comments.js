e9b1950341df8fcbba05127de5313e71f4d64ea9
Osmaldy Maldonado <osmaldy11checo@gmail.com>
2023-04-11 00:30:18 -0400

    Updating comment schema

--- models/Comments.js
+++ models/Comments.js
@@ -4,13 +4,13 @@
  * Schema of comments.
  */
 const CommentSchema = {
-  id: Number,
-  id_user: Number,
-  id_post: Number,
-  comment: String,
-  media: String,
-  date_adding: String,
-  date_modified: String,
+  id: 0,
+  id_user: 0,
+  id_post: 0,
+  comment: "",
+  media: "",
+  date_adding: "",
+  date_modified: "",
 };
 
 export default CommentSchema; // Exporting schema.
